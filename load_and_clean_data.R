a4f8ebc32b25e80f148f2532e3ff868eb0d68459
Alyssa Ramoutar <arvm@bu.edu>
2022-11-11 16:25:31 -0500

    Blogpost 5.

--- load_and_clean_data.R
+++ load_and_clean_data.R
@@ -8,3 +8,14 @@ school_scores_data_clean <- school_scores_data
 write_csv(school_scores_data_clean, file = here::here("dataset", "school_scores_data_clean.csv"))
 
 save(school_scores_data_clean, file = here::here("dataset/school_scores.RData"))
+
+library(tidyverse)
+
+census_data <- read_csv(here::here("dataset", "acs2017_census_tract_data.csv"))
+
+## CLEAN the data
+census_data_clean <- census_data
+
+write_csv(census_data_clean, file = here::here("dataset", "acs2017_census_tract_data.csv"))
+
+save(census_data_clean, file = here::here("dataset/census_data.RData"))
