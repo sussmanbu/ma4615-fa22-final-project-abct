54caab6c3c745e0ccf281565fb44540eee3d9af2
Arlo Ramoutar <arvm@bu.edu>
2022-12-14 12:12:14 -0500

    income interactive.

--- shiny_app/interactive2/app.R
+++ shiny_app/interactive2/app.R
@@ -5,47 +5,49 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
+# 
 #
-
+library(tidyverse)
 library(shiny)
+df <- read_csv("incomegenderdata.csv") %>% 
+  pivot_longer(cols = c(total.score, female.total.score, male.total.score), names_to = "gender", values_to = "score") %>%
+  pivot_longer(cols = income.between.20.40k:income.more.than.100k, names_to = "income.bracket", values_to = "scores")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Income Scores"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput(
+        "income.bracket",
+        "Select groups",
+        unique(df$income.bracket),
+        selected = unique(df$income.bracket)[1]
+      )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("barPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$barPlot <- renderPlot({
+    # show data for  input$group from ui.R
+    df %>%
+      filter(income.bracket %in% input$income.bracket) %>% 
+      ggplot(aes(y = state.name, x = scores, color = income.bracket)) +
+      geom_point(position = "dodge")
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
